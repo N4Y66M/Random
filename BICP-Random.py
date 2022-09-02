354a626c1dd306b7cb7b46cf38a12bd7bbb3191f
wish maker <106797830+N4Y66M@users.noreply.github.com>
2022-09-03 00:32:39 +0600

    Add files via upload

---
+++ BICP-Random.py
@@ -0,0 +1,35 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from N4Y66M import menu
+ 
+        menu()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from N4Y66M32 import menu
+        
+        menu()
+        
+
+
+ 
+ 
+ 
